2aa80f5f73e5dff43a51c82755982973c2f0afb2
Smith <smithwebdd@gmail.com>
2020-01-10 00:51:17 -0500

    Implement listing blog entries

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+
+  default_scope { order(created_at: :desc)}
 end
---
+++ spec/features/listing_blog_entry_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "Listing Blog Entries" do
+  before do
+    @blog1 = Blog.create(title: "This is the first", body: "Heres the body of the blog")
+    @blog2 = Blog.create(title: "This is next", body: "Heres the body of the blog")
+
+  end
+
+  scenario "A user lists all blog entries" do
+    visit '/'
+
+    expect(page).to have_content(@blog1.title)
+    expect(page).to have_content(@blog1.body)
+    expect(page).to have_content(@blog2.title)
+    expect(page).to have_content(@blog2.body)
+
+    expect(page).to have_link(@blog1.title)
+    expect(page).to have_link(@blog2.title)
+
+
+  end
+end
